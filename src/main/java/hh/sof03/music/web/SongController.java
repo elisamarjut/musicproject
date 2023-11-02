1df43182df1e86cc05769adbe97c07a1134680f8
elisamarjut <117983133+elisamarjut@users.noreply.github.com>
2023-11-02 10:24:29 +0200

    Lisätty CRUD-ominaisuudet kappalelistaan

--- src/main/java/hh/sof03/music/web/SongController.java
+++ src/main/java/hh/sof03/music/web/SongController.java
@@ -3,8 +3,12 @@ package hh.sof03.music.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hh.sof03.music.domain.GenreRepository;
+import hh.sof03.music.domain.Song;
 import hh.sof03.music.domain.SongRepository;
 
 @Controller
@@ -13,14 +17,48 @@ public class SongController {
     @Autowired
     private SongRepository songRepository;
 
-    @RequestMapping("index")
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @GetMapping({ "/", "index" })
     public String welcome() {
         return "welcome";
     }
 
-    @RequestMapping("/songlist")
+    // List all songs
+    @GetMapping("/songlist")
     public String listSongs(Model model) {
         model.addAttribute("songs", songRepository.findAll());
         return "songlist";
+    }
+
+    // Add a new song
+    @GetMapping("/addsong")
+    public String addSong(Model model) {
+        model.addAttribute("song", new Song());
+        model.addAttribute("genres", genreRepository.findAll());
+        return "addsong";
+    }
+
+    // Save song
+    @PostMapping("/savesong")
+    public String saveSong(Song song) {
+        songRepository.save(song);
+        return "redirect:/songlist";
+    }
+
+    // Delete song
+    @GetMapping("/deletesong/{id}")
+    public String deleteSong(@PathVariable("id") Long songId, Model model) {
+        songRepository.deleteById(songId);
+        return "redirect:../songlist";
+    }
+
+    // Edit song
+    @GetMapping("/editsong/{id}")
+    public String editSong(@PathVariable("id") Long songId, Model model) {
+        model.addAttribute("song", songRepository.findById(songId));
+        model.addAttribute("genres", genreRepository.findAll());
+        return "editsong";
     }
 }
