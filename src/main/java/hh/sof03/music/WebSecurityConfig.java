eff711a3fa172231e050cbd94aa6edec834436a7
elisamarjut <117983133+elisamarjut@users.noreply.github.com>
2023-11-15 16:07:57 +0200

    Muokattu requestMatchers

--- src/main/java/hh/sof03/music/WebSecurityConfig.java
+++ src/main/java/hh/sof03/music/WebSecurityConfig.java
@@ -21,38 +21,35 @@ import static org.springframework.boot.autoconfigure.security.servlet.PathReques
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
-    @Autowired
-    private UserDetailServiceImpl userDetailsService;
+        @Autowired
+        private UserDetailServiceImpl userDetailsService;
 
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(antMatcher("/css/**")).permitAll()
-                        .requestMatchers(toH2Console()).permitAll()
-                        .requestMatchers(antMatcher("/")).permitAll()
-                        .requestMatchers(antMatcher("/index")).permitAll()
-                        .requestMatchers(antMatcher("/list/**")).permitAll()
-                        .requestMatchers(antMatcher("/add/**")).hasRole("ADMIN")
-                        .requestMatchers(antMatcher("/edit/**")).hasRole("ADMIN")
-                        .anyRequest().authenticated())
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers(toH2Console()))
-                .headers(headers -> headers
-                        .frameOptions(frameoptions -> frameoptions
-                                .disable()))
-                .formLogin(formlogin -> formlogin
-                        .defaultSuccessUrl("/index", true)
-                        .permitAll())
-                .logout(logout -> logout
-                        .permitAll()
-                        .logoutSuccessUrl("/"));
+        @Bean
+        public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests(authorize -> authorize
+                                                .requestMatchers(antMatcher("/css/**")).permitAll()
+                                                .requestMatchers(toH2Console()).permitAll()
+                                                .requestMatchers(antMatcher("/")).permitAll()
+                                                .requestMatchers(antMatcher("/index")).permitAll()
+                                                .requestMatchers(antMatcher("/list/**")).permitAll()
+                                                .anyRequest().authenticated())
+                                .csrf(csrf -> csrf
+                                                .ignoringRequestMatchers(toH2Console()))
+                                .headers(headers -> headers
+                                                .frameOptions(frameoptions -> frameoptions.disable()))
+                                .formLogin(formlogin -> formlogin
+                                                .defaultSuccessUrl("/index", true)
+                                                .permitAll())
+                                .logout(logout -> logout
+                                                .permitAll()
+                                                .logoutSuccessUrl("/"));
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-    }
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+                auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+        }
 }
